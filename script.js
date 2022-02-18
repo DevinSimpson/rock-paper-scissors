247525993b83f4b68d8c9a6b0ab0860cf90c1cd5
Devin Simpson <devinsimpsonaz@gmail.com>
2022-02-18 10:47:51 -0700

    Add computerChoice variable with if else statements to determine result

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ Then we assign 1 to 'Rock' 2 to 'Paper' and 3 to 'Scissors'
 We need a variable to hold the result of this function
 */
 
+let computerChoice
+
 function computerPlay() {
 let randomNumber = Math.floor((Math.random() * 3) + 1);
 console.log(randomNumber);
+if (randomNumber = 1) {
+    computerChoice = 'Rock';
+} else if (randomNumber = 2) {
+    computerChoice = 'Paper';
+} else {
+    computerChoice = 'Scissors';
+}
 }
