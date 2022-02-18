c3ef6136226aa8891e8e25209a874d862dd16cda
Devin Simpson <devinsimpsonaz@gmail.com>
2022-02-18 10:59:05 -0700

    Create assignComputerFunction to assign computerChoice result

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ We need a random number generator to pick a number between 1 and 3
 Then we assign 1 to 'Rock' 2 to 'Paper' and 3 to 'Scissors'
 We need a variable to hold the result of this function
 */
-
-let computerChoice
+let randomNumber;
+let computerChoice;
 
 function computerPlay() {
-let randomNumber = Math.floor((Math.random() * 3) + 1);
-console.log(randomNumber);
-if (randomNumber = 1) {
-    computerChoice = 'Rock';
-} else if (randomNumber = 2) {
-    computerChoice = 'Paper';
-} else {
-    computerChoice = 'Scissors';
+  randomNumber = Math.floor((Math.random() * 3) + 1);
+  console.log(randomNumber);
 }
+
+function assignComputerChoice(randomNumber) {
+  if (randomNumber === 1) {
+      computerChoice = 'Rock';
+  } else if (randomNumber === 2) {
+      computerChoice = 'Paper';
+  } else {
+      computerChoice = 'Scissors';
+  }
+  console.log(computerChoice);
 }
+
+/* */
