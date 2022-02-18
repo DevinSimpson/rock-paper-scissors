3195048eab95cfa3f0a611dbc6398695a9bf8c42
Devin Simpson <devinsimpsonaz@gmail.com>
2022-02-18 10:39:52 -0700

    Add function computerPlay

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello world");
+/*Create a function that returns a random result of 'Rock', 'Paper' or 'Scissors' 
+We need a random number generator to pick a number between 1 and 3
+Then we assign 1 to 'Rock' 2 to 'Paper' and 3 to 'Scissors'
+We need a variable to hold the result of this function
+*/
+
+function computerPlay() {
+let randomNumber = Math.floor((Math.random() * 3) + 1);
+console.log(randomNumber);
+}
